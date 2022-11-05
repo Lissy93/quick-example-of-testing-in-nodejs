f882710d0f2cd905d86cc57f111a4dbe5470d229
Brian Robles <brobles39@hotmail.com>
2022-11-05 02:11:15 -0500

    3 commit

--- coveragetest.sh
+++ coveragetest.sh
@@ -3,7 +3,7 @@
 chmod -R 644 /tmp/result*
 covcurrent=`cat /tmp/result`
 #covprev=`cat /tmp/result-moved`
-#covprev=20.4
+covprev=20.4
 echo "Current coverage = $covcurrent; Previous coverage = $covprev;"
 
 if [ ${covcurrent%.*} -eq ${covprev%.*} ] && [ ${covcurrent#*.} \> ${covprev#*.} ] || [ ${covcurrent%.*} -gt ${covprev%.*} ]; then
