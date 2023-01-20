a16bd2ec89524d7b3b8524f3a56410361ebf30f0
vilakshan <vilakshandadwal@gmail.com>
2023-01-20 23:47:11 +0530

    Add webpack to create bundle file

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const webpack = require('webpack');
+//const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+    mode: 'production',
+    devtool: 'source-map',
+    entry: {
+        server: './app.js',
+    },
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    target: 'node',
+    node: {
+        __dirname: false,
+        __filename: false,
+    },
+    //externals: [nodeExternals()],
+    module: {
+        rules: [
+            {
+                exclude: /node_modules/
+            }
+        ]
+    }
+};
